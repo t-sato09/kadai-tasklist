5a734d509fbda1a5113e8bc23f4dd7698540afc0
takuya sato <t-sato@saf-sec.co.jp>
2018-06-22 02:21:27 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,11 +5,14 @@
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}    
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
 
-        {!! Form::submit('作成') !!}
+        {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
 
-    <h1>タスク一覧</h1>
+    <h1>タスクリスト</h1>
 
     @if (count($tasks) > 0)
         <ul>
             @foreach ($tasks as $task)
-                <li>{{ $task->content }}</li>
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status }} > {{ $task->content }}</li>
             @endforeach
         </ul>
     @endif
     
-    @foreach ($tasks as $task)
-                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
-            @endforeach
-            
      {!! link_to_route('tasks.create', '新規タスクの作成') !!}
 
 @endsection
