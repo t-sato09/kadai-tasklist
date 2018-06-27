26570ad0e60b8e2898ea33bb6eaa53800c0b1139
takuya sato <t-sato@saf-sec.co.jp>
2018-06-27 07:37:25 +0000

    heroku up

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -18,7 +18,13 @@
                 @foreach ($tasks as $task)
                 <?php $user = $task->user; ?>
                     <tr>
-                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>
+                        @if (Auth::id() == $task->user_id)
+                            {!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}
+                        @else
+                            {{ $task->id }}
+                        @endif    
+                        </td>
                         <td>{{ $task->status }}</td>
                         <td>{{ $task->content }}</td>
                     </tr>
